0822e1c83fdf1af4d82c26dc3543b3896d908bba
NURISLAMCODE <info.nurislamrajib@gmail.com>
2025-02-01 09:00:01 +0600

    add spouse

--- lib/controllers/profile/profile_controller.dart
+++ lib/controllers/profile/profile_controller.dart
@@ -8,11 +8,13 @@ class ProfileController extends GetxController{
   RxString selectedGender = ''.obs;
   RxString selectedMarried = ''.obs;
   RxString selectedProfession = ''.obs;
+  RxString selectedNationality = ''.obs;
   RxString selectedBank = ''.obs;
 
   final List<String> gender = ['Male'.tr, 'FeMale'.tr, 'Others'.tr];
   final List<String> maritalStatus = ['Married'.tr, 'Un Married'.tr,];
   final List<String> profession = ['Business'.tr, 'Doctor'.tr, "Engineers".tr,"Housewife".tr, "Others".tr];
+  final List<String> nationality = ['Bangaldeshi'.tr, 'Pakisthani'.tr, "Indian".tr,];
 
 
   final List<String> banks = ['Islami Bank'.tr, 'Al Arafa Bank'.tr,'Sonali Bank'.tr,];
